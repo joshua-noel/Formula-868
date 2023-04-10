f3a7551c70b894da3ec0399619dd7a4595c3fbd5
Joshua Noel <noelejoshua@gmail.com>
2023-04-10 13:52:22 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbarMenu');
+const url = "http://ergast.com/api/f1/current/driverStandings.json"; //appends .json to format response as jsom instead of xml
 
-menu.addEventListener('click', function() {
+menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
 
+function drawTable(driverArr) {
+    let table = document.querySelector("tbody#results");
 
-// function drawTable(records){
-//     let result = document.querySelector('#result');
-    
-//     let html='';
+    for (let driver of driverArr) {
+        let row = document.createElement("tr");
+        let driver_cell = document.createElement("td");
+        driver_cell.innerText = `${driver.Driver.givenName} ${driver.Driver.familyName}`;
+        let company_cell = document.createElement("td");
+        company_cell.innerText = `${driver.Constructors[0].name}`;
+        let points_cell = document.createElement("td");
+        points_cell.innerText = `${driver.points}`;
 
-//     for(let rec of records){
-//         let html += 
-//             `<tr>
-//                 <td>${driver.Driver.givenName} ${driver.Driver.familyName}</td>
-//                 <td>${driver.Constructors[0].name}</td>
-//                 <td>${driver.points}</td>
-//             <\tr>`;
-//     }
+        row.appendChild(driver_cell);
+        row.appendChild(company_cell);
+        row.appendChild(points_cell);
+        table.appendChild(row);
+    }
+}
 
-//     result.innerHTML = html;
-//     }
+(async () => {
+    const response = await fetch(url);
+    const json = await response.json();
 
-//     async function getData(){
-//         const response = await fetch('https://ergast.com/api/f1/current/driverStandings');
-//         const data = await response.json();
-//         //drawTable(data.MRData.StandingsTable.StandingsLists[0].DriverStandings.slice(0, 10));
-//         drawTable(data);
-//     }
-
-// getData();
+    drawTable(json.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+})();
